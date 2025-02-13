0b4b579573c67e015945c2aff3d2e11683078873
shrinathmane <shrinathmane43@gmail.com>
2025-02-13 18:31:38 +0530

    Create variable.tf

---
+++ Threetier/variable.tf
@@ -0,0 +1,15 @@
+variable "my_region" {
+  type = string
+}
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
+variable "my_ami" {
+  type = string
+}
+variable "vpc_id" {
+  type = string
+}
